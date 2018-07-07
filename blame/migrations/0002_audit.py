08da1bd67a217c552bb94ac84959b1ec34acf16f
Marco Martinez <m-martinez@users.noreply.github.com>
2018-07-06 17:33:44 -0700

    Add historical audit migration

---
+++ blame/migrations/0002_audit.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+#
+# NOTE: This migration is kept for historical purposes.
+#
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blame', '0001_initial'),
+    ]
+
+    operations = [
+    ]
